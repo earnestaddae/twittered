7b19e7ebe165edcb9cfb22e50eb89ac95c79bbea
Ernest Addae <earnestaddae@gmail.com>
2016-02-19 09:25:50 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Twittered App!"
-      redirect_to @user
+      redirect_to user_path(@user)
     else
       render 'new'
     end
