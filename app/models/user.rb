8bf7301e3434b34a4e48668007f7008f3e90d195
Ernest Addae <earnestaddae@gmail.com>
2016-02-19 09:55:46 +0000

    Complete log in/log in test

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,17 @@ validates :email, presence: true, length: {maximum: 255},
   has_secure_password
   validates :password, length: {minimum: 6}
 
+  class << self
+    # returns the hash digest of the given string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # returns the hash digest of the given string
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # returns a random token
-  def User.new_token
-    SecureRandom.urlsafe_base64
+    # returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # remembers a user in the database for use in persistent sessions
